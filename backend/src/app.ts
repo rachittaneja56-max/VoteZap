200be65b9197a4ed3b5d9610b84904a78a085e70
rachittaneja56-max <rachittaneja56@gmail.com>
2026-05-15 00:10:13 +0530

    added proxy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,7 +27,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
-
+app.set('trust proxy', 1);
 app.get('/health', (req: Request, res: Response) => {
   sendSuccess(res, 'Server is healthy', null);
 });
